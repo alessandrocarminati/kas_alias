192ed49859aed54192481d5a461699095654545a
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-06-23 20:10:30 +0000

    nm data from file
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- main.c
+++ main.c
@@ -15,68 +15,39 @@ void find_suffix(const char *name, const char *suffix, char *output_suffix){
 
 
 
-int main() {
+int main(int argc, char *argv[]) {
 	struct item *head = {NULL};
 	struct item *o_head = {NULL};
 	struct item  *current;
 	struct duplicate_item *duplicate, *duplicate_iterator;
 	struct linker_object *lod, *lod_iterator;
-	uint64_t	address;
-	char		t, sym_name[MAX_NAME_SIZE], new_name[MAX_NAME_SIZE];
+	uint64_t address;
+	int verbose_mode = 0;
+	char t, sym_name[MAX_NAME_SIZE], new_name[MAX_NAME_SIZE];
+	FILE *fp;
 
-	printf("fetching loader producted map\n");
-	lod=parseLinkerObjects("vmlinux.map");
-/*
-	lod_iterator=lod;
-	while (lod_iterator != NULL) {
-		printf("type=%s, address=0x%08lx, size=0x%08lx, filename=%s\n", lod_iterator->type, lod_iterator->address, lod_iterator->size, lod_iterator->filename);
-		lod_iterator=lod_iterator->next;
+	if (argc < 3 || argc > 4) {
+		printf("Usage: %s <nmfile> <vmlinux.map> [-verbose]\n", argv[0]);
+		return 1;
 		}
-*/
-//ffffffff814c8f10 T dev_fwnode
+	if (argc == 4 && strcmp(argv[3], "-verbose") == 0) {
+		verbose_mode = 1;
+		}
 
-//	printf("symbol=dev_fwnode address=0xffffffff814c8f10 filename=%s\n", addr2filename(lod, 0xffffffff814c8f10 ));
+	if (verbose_mode) printf("fetching loader producted map (%s)\n", argv[2]);
+	lod=parseLinkerObjects(argv[2]);
 
-	printf("Scanning nm data\n");
-	while (scanf("%lx %c %99s\n", &address, &t, sym_name) == 3) {
+	if (verbose_mode) printf("Scanning nm data(%s)\n", argv[1]);
+	fp = fopen (argv[1], "r");
+	while (fscanf(fp, "%lx %c %99s\n", &address, &t, sym_name) == 3) {
 		addItem(&head, sym_name, t, address);
 		}
-
-
-/*	current = head;
-	while (current != NULL) {
-		printf("%08lx %c %s\n", current->addr, current->stype, current->symb_name);
-		current = current->next;
-		}
-*/
-	printf("Sorting nm data\n");
+	fclose(fp);
+	if (verbose_mode) printf("Sorting nm data\n");
 	sortList(&head, BY_NAME);
-/*
-	current = head;
-	while (current != NULL) {
-		printf("%08lx %c %s\n", current->addr, current->stype, current->symb_name);
-		current = current->next;
-		}
-*/
-//	if (!insert_after(head, 0xffffffff811c9a50, "zone_watermark_ok_stocazzo", 0xffffffff811c9a50,'T')) return 1;
-/*
-	current = head;
-	while (current != NULL) {
-		printf("%08lx %c %s\n", current->addr, current->stype, current->symb_name);
-		current = current->next;
-		}
-*/
-	printf("Scanning nm data for duplicates\n");
+	if (verbose_mode) printf("Scanning nm data for duplicates\n");
 	duplicate = findDuplicates(head);
-/*
-	duplicate_iterator=duplicate;
-	while (duplicate_iterator != NULL) {
-		printf("%08lx %c %s\n", duplicate_iterator->original_item->addr, duplicate_iterator->original_item->stype, duplicate_iterator->original_item->symb_name);
-		duplicate_iterator=duplicate_iterator->next;
-		}
-*/
-
-	printf("Applying suffixes\n");
+	if (verbose_mode) printf("Applying suffixes\n");
 	duplicate_iterator=duplicate;
 	while (duplicate_iterator != NULL) {
 			find_suffix(duplicate_iterator->original_item->symb_name, addr2filename(lod, 0xffffffff814c8f10), new_name);
