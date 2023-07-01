ae56d0b6167bba4cfde06841fae724b229cbb611
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-07-01 11:08:24 +0000

    Prevent reprocessing on second kallsyms pass
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- kas_alias.c
+++ kas_alias.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
 	uint64_t address;
 	int verbose_mode = 0;
 	char t, sym_name[MAX_NAME_SIZE], new_name[MAX_NAME_SIZE];
+	bool need_2_process=true;
 	FILE *fp;
 
 	if (argc < 2 || argc > 3) {
@@ -37,30 +38,36 @@ int main(int argc, char *argv[])
 		printf("Scanning nm data(%s)\n", argv[1]);
 	fp = fopen(argv[1], "r");
 	while (fscanf(fp, "%lx %c %99s\n", &address, &t, sym_name) == 3) {
+		if (strstr(sym_name, "@1") != NULL) {
+			if (verbose_mode && need_2_process) printf("Already processed\n");
+			need_2_process=false;
+			}
 		last = add_item(&last, sym_name, t, address);
 		if (!head)
 			head = last;
 	}
 	fclose(fp);
-	if (verbose_mode)
-		printf("Sorting nm data\n");
-	sort_list_m(&head, BY_NAME);
-	if (verbose_mode)
-		printf("Scanning nm data for duplicates\n");
-	duplicate = find_duplicates(head);
-	if (verbose_mode)
-		printf("Applying suffixes\n");
-	build_index(head);
-	duplicate_iterator = duplicate;
-	while (duplicate_iterator) {
-		create_suffix(duplicate_iterator->original_item->symb_name, new_name);
-		if (!insert_after(head, duplicate_iterator->original_item->addr, new_name,
-				  duplicate_iterator->original_item->addr,
-				  duplicate_iterator->original_item->stype))
-			return 1;
-		duplicate_iterator = duplicate_iterator->next;
+	if (need_2_process) {
+		if (verbose_mode)
+			printf("Sorting nm data\n");
+		sort_list_m(&head, BY_NAME);
+		if (verbose_mode)
+			printf("Scanning nm data for duplicates\n");
+		duplicate = find_duplicates(head);
+		if (verbose_mode)
+			printf("Applying suffixes\n");
+		build_index(head);
+		duplicate_iterator = duplicate;
+		while (duplicate_iterator) {
+			create_suffix(duplicate_iterator->original_item->symb_name, new_name);
+			if (!insert_after(head, duplicate_iterator->original_item->addr, new_name,
+					  duplicate_iterator->original_item->addr,
+					  duplicate_iterator->original_item->stype))
+				return 1;
+			duplicate_iterator = duplicate_iterator->next;
+		}
+		sort_list_m(&head, BY_ADDRESS);
 	}
-	sort_list_m(&head, BY_ADDRESS);
 	current = head;
 	while (current) {
 		printf("%08lx %c %s\n", current->addr, current->stype, current->symb_name);
