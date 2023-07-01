51e7742301240227e14401dbb211623c08e66fd7
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-07-01 14:41:46 +0000

    prevent last duplicate from being discarded
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- duplicates_list.c
+++ duplicates_list.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "item_list.h"
 #include "duplicates_list.h"
@@ -19,9 +20,11 @@ struct duplicate_item *find_duplicates(struct item *list)
 	struct item *current_item = list;
 	struct item *prev_item = NULL;
 	struct duplicate_item *new_duplicate;
+	bool   prev_was_duplicate = false;
 
 	while (current_item) {
-		if (prev_item && strcmp(current_item->symb_name, prev_item->symb_name) == 0) {
+		if (prev_item && (strcmp(current_item->symb_name, prev_item->symb_name) == 0) ||
+		    prev_was_duplicate) {
 			if (!duplicates) {
 #ifdef DEBUG
 				duplicates_alloc_cnt++;
@@ -31,6 +34,10 @@ struct duplicate_item *find_duplicates(struct item *list)
 				duplicates->original_item = prev_item;
 				duplicates->next = NULL;
 				current_duplicate = duplicates;
+				if (prev_was_duplicate)
+					prev_was_duplicate = false;
+				else
+					prev_was_duplicate = true;
 			} else {
 #ifdef DEBUG
 				duplicates_alloc_cnt++;
@@ -41,6 +48,10 @@ struct duplicate_item *find_duplicates(struct item *list)
 				new_duplicate->next = NULL;
 				current_duplicate->next = new_duplicate;
 				current_duplicate = new_duplicate;
+				if (prev_was_duplicate)
+					prev_was_duplicate = false;
+				else
+					prev_was_duplicate = true;
 			}
 		}
 
