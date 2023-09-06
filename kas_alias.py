3a02c10fdd218cca14169add528028b52f343d6f
Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>
2023-09-06 07:45:03 +0000

    refactor1
    Signed-off-by: Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>

--- kas_alias.py
+++ kas_alias.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2019-2023 Red Hat, Inc. Alessandro Carminati <alessandro.carminati@gmail.com>
+#
+# kas_alias: Adds alias to duplicate symbols in the kallsyms output
+#
+# For further information, see:
+# Documentation/..
+
 import subprocess
 import sys
 import os
+import argparse
+from collections import namedtuple
 
-class Line:
-    def __init__(self, address, type, name):
-        self.address = address
-        self.type = type
-        self.name = name
+Line = namedtuple('Line', ['address', 'type', 'name'])
 
 def parse_file(filename):
     symbol_list = []
@@ -17,12 +25,8 @@ def parse_file(filename):
             fields = line.strip().split()
 
             if len(fields) >= 3:
-                address = fields[0]
-                type = fields[1]
-                name = ' '.join(fields[2:])
-
-                line = Line(address, type, name)
-                symbol_list.append(line)
+                address, type, name = fields[0], fields[1], ' '.join(fields[2:])
+                symbol_list.append(Line(address, type, name))
                 name_occurrences[name] = name_occurrences.get(name, 0) + 1
 
     return symbol_list, name_occurrences
@@ -34,8 +38,7 @@ def find_duplicate(symbol_list, name_occurrences):
     for line in symbol_list:
         if line.name in name_to_lines:
             first_occurrence = name_to_lines[line.name]
-            duplicate_lines.append(first_occurrence)
-            duplicate_lines.append(line)
+            duplicate_lines.extend([first_occurrence, line])
         else:
             name_to_lines[line.name] = line
 
@@ -44,10 +47,10 @@ def find_duplicate(symbol_list, name_occurrences):
 def start_addr2line_process(binary_file, addr2line_file):
     try:
         addr2line_process = subprocess.Popen([addr2line_file, '-fe', binary_file],
-                                            stdin=subprocess.PIPE,
-                                            stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE,
-                                            text=True)
+                                             stdin=subprocess.PIPE,
+                                             stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE,
+                                             text=True)
         return addr2line_process
     except Exception as e:
         print(f"Error starting addr2line process: {str(e)}")
@@ -68,70 +71,35 @@ def fetch_line_from_addr2line(addr2line_process):
         print(f"Error fetching data from addr2line: {str(e)}")
         return None
 
-class Configuration:
-    def __init__(self):
-        self.addr2line_file = None
-        self.vmlinux_file = None
-        self.output_file = None
-        self.nm_data_file = None
-        self.linux_base_dir = None
-
-def parse_command_line(args):
-    config = Configuration()
-    i = 1
-
-    required_options = ['addr2line_file', 'vmlinux_file', 'output_file', 'nm_data_file']
-
-    while i < len(args):
-        if args[i] == '-a' and i + 1 < len(args):
-            config.addr2line_file = args[i + 1]
-            i += 1
-        elif args[i] == '-v' and i + 1 < len(args):
-            config.vmlinux_file = args[i + 1]
-            i += 1
-        elif args[i] == '-o' and i + 1 < len(args):
-            config.output_file = args[i + 1]
-            i += 1
-        elif args[i] == '-n' and i + 1 < len(args):
-            config.nm_data_file = args[i + 1]
-            i += 1
-        else:
-            return None
-
-        i += 1
-
-    for option in required_options:
-        if getattr(config, option) is None:
-            return None
-
-    if config.vmlinux_file:
-        config.linux_base_dir = os.path.dirname(config.vmlinux_file)
-
-    return config
-
 if __name__ == "__main__":
-    command_line_args = sys.argv
-    config = parse_command_line(command_line_args)
+    parser = argparse.ArgumentParser(description='Add alias to multiple occurring symbols name in kallsyms')
+    parser.add_argument('-a', "--addr2line", dest="addr2line_file", required=True)
+    parser.add_argument('-v', "--vmlinux", dest="vmlinux_file", required=True)
+    parser.add_argument('-o', "--outfile", dest="output_file", required=True)
+    parser.add_argument('-n', "--nmdata", dest="nm_data_file", required=True)
+    config = parser.parse_args()
 
-    if config is None:
-        raise SystemExit("args wrong")
+    try:
+        config.linux_base_dir = os.path.dirname(config.vmlinux_file)
+        symbol_list, name_occurrences = parse_file(config.nm_data_file)
+        addr2line_process = start_addr2line_process(config.vmlinux_file, config.addr2line_file)
 
+        with open(config.output_file, 'w') as file:
+            for obj in symbol_list:
+                file.write("{} {} {}\n".format(obj.address, obj.type, obj.name))
+                if obj.type == "T" or obj.type == "t":
+                    if name_occurrences[obj.name] > 1:
+                        send_line_to_addr2line(addr2line_process, obj.address)
+                        output = fetch_line_from_addr2line(addr2line_process)
+                        output = fetch_line_from_addr2line(addr2line_process)
+                        decoration = "@" + "".join(["_" if not c.isalnum() else c for c in output.replace(config.linux_base_dir, "")])
+                        file.write("{} {} {}\n".format(obj.address, obj.type, obj.name + decoration))
 
-    symbol_list, name_occurrences = parse_file(config.nm_data_file)
-    addr2line_process = start_addr2line_process(config.vmlinux_file, config.addr2line_file)
+        addr2line_process.stdin.close()
+        addr2line_process.stdout.close()
+        addr2line_process.stderr.close()
+        addr2line_process.wait()
 
-    with open(config.output_file, 'w') as file:
-       for obj in symbol_list:
-           file.write("{} {} {}\n".format(obj.address, obj.type, obj.name))
-           if obj.type == "T" or obj.type == "t":
-                if name_occurrences[obj.name]>1 :
-                    send_line_to_addr2line(addr2line_process, obj.address)
-                    output = fetch_line_from_addr2line(addr2line_process)
-                    output = fetch_line_from_addr2line(addr2line_process)
-                    decoration = "@" + "".join(["_" if not c.isalnum() else c for c in output.replace(config.linux_base_dir, "")])
-                    file.write("{} {} {}\n".format(obj.address, obj.type, obj.name+decoration))
-
-    addr2line_process.stdin.close()
-    addr2line_process.stdout.close()
-    addr2line_process.stderr.close()
-    addr2line_process.wait()
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise SystemExit("Script terminated due to an error")
