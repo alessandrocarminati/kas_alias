f47f8bea2bc5140aacd9871f78ad38d5a3858e41
Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>
2023-09-06 09:12:16 +0000

    python refactor 2
    Signed-off-by: Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>

--- kas_alias.py
+++ kas_alias.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0-only
 #
-# Copyright (C) 2019-2023 Red Hat, Inc. Alessandro Carminati <alessandro.carminati@gmail.com>
+# Copyright (C) 2023 Red Hat, Inc. Alessandro Carminati <alessandro.carminati@gmail.com>
 #
-# kas_alias: Adds alias to duplicate symbols in the kallsyms output
-#
-# For further information, see:
-# Documentation/..
+# kas_alias: Adds alias to duplicate symbols in the kallsyms output.
 
 import subprocess
 import sys
 import os
 import argparse
+import re
 from collections import namedtuple
+
+regex_list = [
+        "^__cfi_.*$",
+        "^_*TRACE_SYSTEM.*$",
+        "^__already_done\\.[0-9]+$",
+        "^___tp_str\\.[0-9]+$",
+        "^___done\\.[0-9]+$",
+        "^__print_once\\.[0-9]+$",
+        "^_rs\\.[0-9]+$",
+        "^__compound_literal\\.[0-9]+$",
+        "^___once_key\\.[0-9]+$",
+        "^__func__\\.[0-9]+$",
+        "^__msg\\.[0-9]+$",
+        "^CSWTCH\\.[0-9]+$",
+        "^__flags\\.[0-9]+$",
+        "^__wkey.*$",
+        "^__mkey.*$",
+        "^__key.*$",
+        "^__pfx_.*$"
+]
+
+class SeparatorType:
+    def __call__(self, separator):
+        if len(separator) != 1:
+            raise argparse.ArgumentTypeError("Separator must be a single character")
+        return separator
 
 Line = namedtuple('Line', ['address', 'type', 'name'])
 
@@ -56,20 +80,23 @@ def start_addr2line_process(binary_file, addr2line_file):
         print(f"Error starting addr2line process: {str(e)}")
         return None
 
-def send_line_to_addr2line(addr2line_process, line):
+def addr2line_fetch_address(addr2line_process, address):
     try:
-        addr2line_process.stdin.write(line + '\n')
+        addr2line_process.stdin.write(address + '\n')
         addr2line_process.stdin.flush()
-    except Exception as e:
-        print(f"Error sending data to addr2line: {str(e)}")
+        addr2line_process.stdout.readline().strip()
+        output = addr2line_process.stdout.readline().strip()
 
-def fetch_line_from_addr2line(addr2line_process):
-    try:
-        line = addr2line_process.stdout.readline().strip()
-        return line
+        return output
     except Exception as e:
-        print(f"Error fetching data from addr2line: {str(e)}")
+        print(f"Error communicating with addr2line: {str(e)}")
         return None
+
+def process_line(line, config):
+    if config:
+        return obj.type == "T" or obj.type == "t" or (obj.type == "D" or obj.type == "d" and any(re.match(regex, obj.name) for regex in regex_list))
+    else:
+        return obj.type == "T" or obj.type == "t"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Add alias to multiple occurring symbols name in kallsyms')
@@ -77,6 +104,8 @@ if __name__ == "__main__":
     parser.add_argument('-v', "--vmlinux", dest="vmlinux_file", required=True)
     parser.add_argument('-o', "--outfile", dest="output_file", required=True)
     parser.add_argument('-n', "--nmdata", dest="nm_data_file", required=True)
+    parser.add_argument('-s', "--separator", dest="separator", required=False, default="@", type=SeparatorType())
+    parser.add_argument('-d', "--data", dest="include_data", required=False, action='store_true')
     config = parser.parse_args()
 
     try:
@@ -87,13 +116,12 @@ if __name__ == "__main__":
         with open(config.output_file, 'w') as file:
             for obj in symbol_list:
                 file.write("{} {} {}\n".format(obj.address, obj.type, obj.name))
-                if obj.type == "T" or obj.type == "t":
+                if process_line(obj, config.include_data) :
                     if name_occurrences[obj.name] > 1:
-                        send_line_to_addr2line(addr2line_process, obj.address)
-                        output = fetch_line_from_addr2line(addr2line_process)
-                        output = fetch_line_from_addr2line(addr2line_process)
-                        decoration = "@" + "".join(["_" if not c.isalnum() else c for c in output.replace(config.linux_base_dir, "")])
-                        file.write("{} {} {}\n".format(obj.address, obj.type, obj.name + decoration))
+                        output = addr2line_fetch_address(addr2line_process, obj.address)
+                        decoration = config.separator + "".join(["_" if not c.isalnum() else c for c in output.replace(config.linux_base_dir, "")])
+                        if decoration != config.separator + "____":
+                            file.write("{} {} {}\n".format(obj.address, obj.type, obj.name + decoration))
 
         addr2line_process.stdin.close()
         addr2line_process.stdout.close()
