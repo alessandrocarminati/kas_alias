9091ed3a833fb341618d1cb9ac7978501b093953
Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>
2023-10-19 14:45:06 +0000

    module extension 3nd draft
    Signed-off-by: Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>

--- kas_alias.py
+++ kas_alias.py
@@ -43,11 +43,12 @@ class Addr2LineError(Exception):
 Line = namedtuple('Line', ['address', 'type', 'name'])
 
 # Parses a given nm output and returns the symbol list along with a hash of symbol occurrences
-def parse_nm_lines(lines, symbol_list=None, name_occurrences=None):
-    if symbol_list is None:
-        symbol_list = []
+def parse_nm_lines(lines, name_occurrences=None):
+
     if name_occurrences is None:
         name_occurrences = {}
+
+    symbol_list = []
 
     for line in lines:
         fields = line.strip().split()
@@ -94,7 +95,8 @@ def process_line(obj, process_data_sym):
 def fetch_file_lines(filename):
     try:
         with open(filename, 'r') as file:
-            lines = file.readlines()
+#            lines = file.readlines()
+            lines = [line.strip() for line in file.readlines()]
         return lines
     except FileNotFoundError:
         raise FileNotFoundError(f"File not found: {filename}")
@@ -106,15 +108,17 @@ def do_nm(filename, nm_executable):
         return nm_output
     except subprocess.CalledProcessError as e:
         print(f"Error executing 'nm' command: {e.output}")
-        return []
+        print(f"pwd={os.getcwd()}")
+        print(f"filename={filename}")
+        raise SystemExit("Script terminated due to an error")
 
 # Accepts a Line object and a decoration string, then generates an argument to be used with objcopy for
 # adding an alias to the module's object file
 def make_objcpy_arg(obj, decoration):
-    section = ".text" if obj.type.upper() == "T" else (".rodata" if obj.type.upper() == "D" else ".bss")
+    section = ".text" if obj.type.upper() == "T" else (".data" if obj.type.upper() == "D" else (".rodata" if obj.type.upper() == "R" else ".bss"))
     flag = "global" if obj.type.isupper() else "local"
 
-    return f"--add-symbol {obj.name + decoration}={section}:{obj.address},{flag} "
+    return f"--add-symbol {obj.name + decoration}={section}:0x{obj.address},{flag} "
 
 # Adds aliases to a given module's object file by executing objcopy
 def execute_objcopy(objcopy_executable, objcopy_args, object_file):
@@ -141,17 +145,19 @@ def generate_decoration(obj, config, addr2line_process):
     # specified address in the DWARF section that after normalization it becomes "____".
     # In such cases, emitting an alias wouldn't make sense, so it is skipped.
     if decoration != config.separator + "____":
-       return ""
-    return decoration
+       return decoration
+    return ""
 
 # Creates a new module object file with added aliases in the ELF .symtab
 def produce_output_modules(config, symbol_list, name_occurrences, module_file_name, addr2line_process):
+    objcopy_args = "";
     for obj in symbol_list:
         if (name_occurrences[obj.name] > 1) and process_line(obj, config.process_data_sym):
             decoration = generate_decoration(obj, config, addr2line_process)
             if decoration != "":
                 objcopy_args = objcopy_args + make_objcpy_arg(obj, decoration)
 
+    print(f"kas_alias: {config.objcopy_file} {objcopy_args} {module_file_name}.bak {module_file_name}")
     execute_objcopy(config.objcopy_file, objcopy_args, module_file_name)
 
 # Generates a new file containing nm data for vmlinux with the added aliases
@@ -164,62 +170,78 @@ def produce_output_vmlinux(config, symbol_list, name_occurrences, addr2line_proc
                 if decoration != "":
                     output_file.write(f"{obj.address} {obj.type} {obj.name + decoration}\n")
 
-    close(config.output_file)
+# Copies a file using the sell
+def copy_file(source, destination):
+    copy_command = f"cp {source} {destination}"
+
+    try:
+        subprocess.check_output(copy_command, shell=True)
+    except subprocess.CalledProcessError:
+        print(f"Error copying {source_file} to {destination_file}.")
+        raise SystemExit("Script terminated due to an error")
 
 if __name__ == "__main__":
     # Handles command-line arguments and generates a config object
     parser = argparse.ArgumentParser(description='Add alias to multiple occurring symbols name in kallsyms')
     parser.add_argument('-a', "--addr2line", dest="addr2line_file", required=True)
-    parser.add_argument('-n', "--nm", dest="nm_file", required=True)
-    parser.add_argument('-c', "--objcopy", dest="objcopy_file", required=True)
-    parser.add_argument('-v', "--vmlinux", dest="vmlinux_file", required=True)
-    parser.add_argument('-o', "--outfile", dest="output_file", required=True)
-    parser.add_argument('-n', "--nmdata", dest="nm_data_file", required=True)
     parser.add_argument('-b', "--basedir", dest="linux_base_dir", required=True)
-    parser.add_argument('-m', "--modules_list", dest="module_list", required=True)
-    parser.add_argument('-s', "--separator", dest="separator", required=False, default="@", type=SeparatorType())
+    parser.add_argument('-c', "--objcopy", dest="objcopy_file", required=True)
     parser.add_argument('-d', "--process_data", dest="process_data_sym", required=False, action='store_true')
+    parser.add_argument('-e', "--nm", dest="nm_file", required=True)
+    parser.add_argument('-m', "--modules_list", dest="module_list", required=True)
+    parser.add_argument('-n', "--nmdata", dest="nm_data_file", required=True)
+    parser.add_argument('-o', "--outfile", dest="output_file", required=True)
+    parser.add_argument('-s', "--separator", dest="separator", required=False, default="@", type=SeparatorType())
+    parser.add_argument('-v', "--vmlinux", dest="vmlinux_file", required=True)
     config = parser.parse_args()
 
-    # link-vmlinux.sh calls this two times: Avoid running kas_alias twice for efficiency and prevent duplicate aliases
-    #  in module processing by checking the last letter of the nm data file
-    if config.nm_data_file and config.nm_data_file[-1] == '2':
-        try:
-            # Determine kernel source code base directory
-            config.linux_base_dir = os.path.normpath(os.getcwd() + "/" + config.linux_base_dir) + "/"
+    try:
+        print("kas_alias: Start processing")
 
-            # Process nm data from vmlinux
-            vmlinux_nm_lines = fetch_file_lines(config.nm_data_file)
-            vmlinux_symbol_list, name_occurrences = parse_nm_lines(vmlinux_nm_lines)
+        # Determine kernel source code base directory
+        config.linux_base_dir = os.path.normpath(os.getcwd() + "/" + config.linux_base_dir) + "/"
 
-            # Process nm data for modules
-            module_list = fetch_flie_lines(config.module_list)
-            for module in module_list:
-                module_nm_lines = do_nm(module, config.nm_file)
-                module_symbol_list[module], name_occurrences = parse_nm_lines(module_nm_lines, name_occurrences)
+        print("kas_alias: Process nm data from vmlinux")
+        # Process nm data from vmlinux
+        vmlinux_nm_lines = fetch_file_lines(config.nm_data_file)
+        vmlinux_symbol_list, name_occurrences = parse_nm_lines(vmlinux_nm_lines)
 
-            # Produce file for vmlinux
-            addr2line_process = start_addr2line_process(config.vmlinux_file, config.addr2line_file)
-            produce_output_vmlinux(config, vmlinux_symbol_list, name_occurrences, addr2line_process)
-            addr2line_process.stdin.close()
-            addr2line_process.stdout.close()
-            addr2line_process.stderr.close()
-            addr2line_process.wait()
+        print("kas_alias: Process nm data for modules")
+        # Process nm data for modules
+        module_list = fetch_file_lines(config.module_list)
+        module_symbol_list = {}
+        for module in module_list:
+            module_nm_lines = do_nm(module, config.nm_file)
+            module_symbol_list[module], name_occurrences = parse_nm_lines(module_nm_lines, name_occurrences)
 
+        print("kas_alias: Produce file for vmlinux")
+        # Produce file for vmlinux
+        addr2line_process = start_addr2line_process(config.vmlinux_file, config.addr2line_file)
+        produce_output_vmlinux(config, vmlinux_symbol_list, name_occurrences, addr2line_process)
+        addr2line_process.stdin.close()
+        addr2line_process.stdout.close()
+        addr2line_process.stderr.close()
+        addr2line_process.wait()
+
+        # link-vmlinux.sh calls this two times: Avoid running kas_alias twice for efficiency and prevent duplicate aliases
+        #  in module processing by checking the last letter of the nm data file
+        if config.vmlinux_file and config.vmlinux_file[-1] == '2':
+            print("kas_alias: Add aliases to module files")
             # Add aliases to module files
             for module in module_list:
                 addr2line_process = start_addr2line_process(module, config.addr2line_file)
-                produce_output_modules(config, module_symbol_list[module], name_occurrences, addr2line_process)
+                produce_output_modules(config, module_symbol_list[module], name_occurrences, module, addr2line_process)
                 addr2line_process.stdin.close()
                 addr2line_process.stdout.close()
                 addr2line_process.stderr.close()
                 addr2line_process.wait()
+        else:
+            print("kas_alias: Skip module processing if pass is different from second")
 
-        except Addr2LineError as e:
-            print(f"An error occurred in addr2line: {str(e)}")
-            raise SystemExit("Script terminated due to an error")
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            raise SystemExit("Script terminated due to an error")
-    else:
-        shutil.copy(config.nm_data_file, config.output_file)
+
+    except Addr2LineError as e:
+        print(f"An error occurred in addr2line: {str(e)}")
+        raise SystemExit("Script terminated due to an error")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise SystemExit("Script terminated due to an error")
